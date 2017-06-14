dab5c9031ac0a9f5962c9ed24410120abe61f9c6
jinwei.sun <jinwei.sun@fu-fang.com>
2017-06-14 18:41:03 +0800

    添加最简单的chrome 插件

---
+++ src/main/webapp/chrome-script/fillUser.js
@@ -0,0 +1,4 @@
+alert("I Love Xiao");
+document.getElementById("user[login]").value = "xiaoma2";
+document.getElementById("user[email]").value = "hacker_ma@163.com";
+document.getElementById("user[password]").value = "123456";
