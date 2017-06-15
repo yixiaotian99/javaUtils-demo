132f0c1d5b5fe6d50ceda6f5aa10df0cb36fefbe
jinwei.sun <jinwei.sun@fu-fang.com>
2017-06-15 10:33:10 +0800

    添加浏览器插件定时点击

--- src/main/webapp/chrome-script/simple2/fillUser.js
+++ src/main/webapp/chrome-script/simple2/fillUser.js
@@ -1,4 +1,14 @@
-$("#login_username").val("haha");
-$("#login_password").val("111111");
-$("#loginBtn").click();
+var count=1
+function mySumbit(){
+    console.log(count++)
+    $("#login_username").val("haha2222");
+    $("#login_password").val("111111");
+    $("#loginBtn").click();
+}
 
+//每隔3秒就执行一次
+window.setInterval(mySumbit, 3000)
+
+
+//延时3秒后执行一次
+//window.setTimeout(mySumbit, 3000)
