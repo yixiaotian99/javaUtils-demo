283758d550f424099c11d74ff4f03d3eb90fbf8d
jinwei.sun <jinwei.sun@fu-fang.com>
2017-09-15 16:06:52 +0800

    添加泛型的学习使用

--- src/main/java/com/my/basic/DataStructure.java
+++ src/main/java/com/my/basic/DataStructure.java
@@ -174,4 +174,124 @@ class PropertiesDemo{
 }
 
 
+/**
+ * Collections
+ */
+class CollectionsDemo{
+    public void test(){
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(1);
+        list.add(5);
+
+//        Collections.sort(list);
+//        System.out.println(list); //[1, 2, 5]
+
+//        System.out.println(Collections.min(list)); //输出 1
+
+        ArrayList<Integer> list2 = new ArrayList<>(Arrays.asList(new Integer[list.size()])); //必须使用这种方式初始化，否则会报 not fit 错误
+        Collections.copy(list2, list);
+        System.out.println(list2);  //[2, 1, 5]
+    }
+
+    public static void main(String[] args) {
+        CollectionsDemo demo = new CollectionsDemo();
+        demo.test();
+    }
+}
+
+
+/**
+ * 泛型方法
+ */
+class GenericsFunctionDemo{
+
+    public static void main(String[] args) {
+//        Integer[] array = {1,3,5};
+//        String[] array2 = new String[]{"zhangsan", "lisi", "wangwu"};
+//        printArray(array);
+//        printArray(array2);
+
+//        getMax(2, 1, 6);
+//        getMax(2.2, 5.3, 1.8);
+//        getMax("apple", "orange", "banana");
+
+        List<Integer> intList = new ArrayList<>();
+        List<String> stringList = new ArrayList<>();
+        getData(intList);
+        //getData(stringList);  调用报错，不兼容? extends Number 只能为 Number 或子类
+    }
+
+    //泛型方法： 参数使用 E 来表示  在返回类型前使用 <E> 标志 表示 element 元素
+    public static <E> void printArray(E[] array){
+        for(E element : array){
+            System.out.println(element);
+        }
+    }
+
+    //返回最大值 使用 extends 表示继承
+    public static <T extends Comparable<T>> T getMax(T a, T b, T c){
+        T max = a;   //默认第一个数为最大值
+        //System.out.println(b.compareTo(max));
+        if( b.compareTo(max) > 0){
+            max = b;
+        }
+
+        if( c.compareTo(max) > 0){
+            max = c;
+        }
+        System.out.println(String.format("最大值: %s", max));
+        return max;
+    }
+
+    //限定只定的类型
+    public static <T> void getData(List<? extends Number> data){  //这里只能是任意类型 ? 来继承，不能使用 T 明确类型
+        System.out.println("data: " + data);
+    }
+}
+
+
+/**
+ * 泛型类
+ */
+class GenericsClassDemo <T>{
+    private T t;
+
+    public T getT() {
+        return t;
+    }
+
+    public void setT(T t) {
+        this.t = t;
+    }
+
+    @Override
+    public String toString() {
+        return "GenericsClassDemo{" +
+                "t=" + t +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        GenericsClassDemo<Integer> integerDemo = new GenericsClassDemo<>();
+        GenericsClassDemo<String> stringDemo = new GenericsClassDemo<>();
+
+        integerDemo.t = 123;
+        stringDemo.t = "zhangsan";
+
+        System.out.println(integerDemo);
+        System.out.println(stringDemo);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
 
