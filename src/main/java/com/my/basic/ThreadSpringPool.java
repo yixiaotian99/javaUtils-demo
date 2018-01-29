b97bab3cfd508d7eb70ca0ebff23f1a322bc6645
sunjinwei <sunjinwei@jd.com>
2018-01-29 20:03:09 +0800

    添加 spring thread pool 学习

---
+++ src/main/java/com/my/basic/ThreadSpringPool.java
@@ -0,0 +1,37 @@
+package com.my.basic;
+
+import com.xiaoleilu.hutool.thread.ThreadUtil;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by sunjinwei on 2018/1/29.
+ *
+ * @author sunjinwei
+ *         <p>
+ *         创建 spring thread pool
+ */
+public class ThreadSpringPool {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("spring-threadPool.xml");
+        ThreadPoolTaskExecutor executor = (ThreadPoolTaskExecutor) classPathXmlApplicationContext.getBean("executor");
+
+        System.out.println(executor);
+
+        for (int i = 0; i < 27; i++) {
+            executor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(Thread.currentThread().getName() + "-------");
+                    ThreadUtil.sleep(1, TimeUnit.SECONDS);
+                }
+            });
+        }
+
+        System.out.println("done....");
+    }
+
+}
