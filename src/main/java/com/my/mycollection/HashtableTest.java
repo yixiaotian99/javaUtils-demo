6e14cb2092a04a74f25941fff805e8228f331c89
Jinwei Sun <>
2020-03-11 08:58:37 +0800

    添加Hashtable学习

---
+++ src/main/java/com/my/mycollection/HashtableTest.java
@@ -0,0 +1,45 @@
+package com.my.mycollection;
+
+import java.util.Hashtable;
+
+/**
+ * @Author sunjinwei
+ * @Date 2020-03-10 15:21
+ * @Description
+ **/
+public class HashtableTest {
+
+
+    public static void main(String[] args) {
+//        Hashtable ht = new Hashtable(0);
+//        ht.put(null, "");
+
+//        Student s = new Student();
+//        ht.put("s1", s);
+//
+////        System.out.println(ht.entrySet());
+//
+//        System.out.println(Math.abs(Integer.MAX_VALUE));  // 2147483647
+//        System.out.println(Math.abs(Integer.MAX_VALUE + 1));// -2147483648
+//        System.out.println(-1 * (-2147483648)); // -2147483648
+
+
+        Hashtable ht = new Hashtable();
+        ht.put(24, "");
+        ht.put(20, "");
+        ht.put(9, "");
+        ht.put(31, "");
+
+        System.out.println(ht);
+
+        for (Object key : ht.keySet()) {
+            System.out.println(key + "->");
+        }
+    }
+}
+
+//
+//@Data
+//class Student {
+//    private String name;
+//}
